bb939abf4620aaa90ebc6824360033eb6d7af205
SebastianBetancourt <joan.betancourt@correounivalle.edu.co>
2021-02-20 18:46:52 -0500

    cap2

---
+++ cap2/src/MyGame/MyGame.js
@@ -0,0 +1,16 @@
+function MyGame(htmlCanvasID) {
+	 // The shader for drawing
+	 this.mShader = null;
+	 gEngine.Core.initializeWebGL(htmlCanvasID);
+	 this.mShader = new SimpleShader("src/GLSLShaders/SimpleVS.glsl", "src/GLSLShaders/SimpleFS.glsl");
+	 
+	 // Step C1: Clear the canvas
+	 gEngine.Core.clearCanvas([0, 0.8, 0, 1]);
+
+	 // Step C2: Activate the proper shader
+         this.mShader.activateShader([1, 0, 0, 1]);
+	 
+	 // Step C3: Draw with the currently activated geometry and the activated shader
+	 var gl = gEngine.Core.getGL();
+	 gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
